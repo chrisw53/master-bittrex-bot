7a3038b16778384a8b850f59cee934a75513d42f
Chris Wang <chris_wang@icloud.com>
2018-02-15 13:38:56 +0000

    code readability

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const bittrex = require('node.bittrex.api');
-const bot = require('./statics/telegramAPI_KEY');
+const telegram = require('./statics/telegramAPI_KEY');
 
 bittrex.options({
     'apikey': 'YOUR_BITTREX_API_KEY',
@@ -11,7 +11,10 @@ bittrex.options({
     'baseUrl': 'https://bittrex.com/api/v1.1',
 });
 
-bot.on('message', (msg) => {
+/**
+ * Listener function for user telegram function 
+ */
+telegram.on('message', (msg) => {
     const chatId = msg.chat.id;
     const messageFromTelegram = msg.text.split('=');
     const market = messageFromTelegram[1];
@@ -27,7 +30,10 @@ bot.on('message', (msg) => {
 
                 console.log(`Initial quant needed = ${quantNeeded}`);
 
-                // the condition comparison is here
+                /*
+                 Parameter checks are here. This compares daily low with the ask price and
+                 daily high with daily low
+                */
                 if (data.result[0].Ask < (data.result[0].Low * lowCurrentPercent)
                     && data.result[0].High < (data.result[0].Low * highLowPercent)) {
                     bittrex.getorderbook({
@@ -46,16 +52,24 @@ bot.on('message', (msg) => {
                                 const sellPercent = (1 + process.env.sellPercent/100) || 1.10;
 
                                 console.log('Loop happened');
-                                if (array && array.length != 0) {
+                                if (array && array.length !== 0) {
                                     const chunkQuantity = array[0].Quantity;
                                     const buyRate = array[0].Rate;
                                     const sellRate = buyRate * sellPercent * tradeFee;
                                     const quantToBuy = quantNeeded - quantHave;
                                     const isLastLoop = chunkQuantity > quantToBuy;
 
+                                    /*
+                                     If the quantity needed cannot be satisfied by current block, 
+                                     syncLoop is called again to move onto the next block after 
+                                     buying the current block. This is needed because Bittrex API
+                                     v1.1 does not allow market buy and this is to use limit buy
+                                     to simulate market buy by manually fielding Fill or Kill limit
+                                     buy order until the total buy quantity is met.
+                                    */ 
                                     if (!isLastLoop) {
                                         console.log(
-                                            `The quant have vs quant needed: ${quantHave < quantNeeded}quantNeeded: ${quantNeeded} The quantity I have is ${quantHave} The chunk quantity is ${chunkQuantity} It's the last loop: ${isLastLoop} The buy rate is ${buyRate}`
+                                            `The quant have vs quant needed: ${quantHave < quantNeeded} quantNeeded: ${quantNeeded} The quantity I have is ${quantHave} The chunk quantity is ${chunkQuantity} It's the last loop: ${isLastLoop} The buy rate is ${buyRate}`
                                         );
 
                                         bittrex.buylimit({
@@ -68,6 +82,10 @@ bot.on('message', (msg) => {
 
                                                 quantHave = quantHave + chunkQuantity;
 
+                                                /* 
+                                                 Immediately after the buy order is processed, a limit sell
+                                                 order is processed to with a user determined marked up rate
+                                                */
                                                 bittrex.selllimit({
                                                     market: market,
                                                     quantity: chunkQuantity,
@@ -76,11 +94,12 @@ bot.on('message', (msg) => {
                                                     if (data.success) {
                                                         const sellMsg = `A sell order went through. For ${chunkQuantity} ${market} coins Rate is ${sellRate}`;
 
-                                                        bot.sendMessage(chatId, sellMsg);
+                                                        telegram.sendMessage(chatId, sellMsg);
 
                                                         console.log(sellMsg);
 
                                                         array.shift();
+                                                        // syncLoop is recalled here
                                                         syncLoop(array, quantHave);
                                                     } else if (!data.success) {
                                                         console.log(`sell failed: ${data.message}`);
@@ -92,13 +111,18 @@ bot.on('message', (msg) => {
                                                 });
                                             } else if (!data.success) {
                                                 console.log(`buy failed: ${data.message}`);
-                                                bot.sendMessage(chatId, `buy failed: ${data.message}`);
+                                                telegram.sendMessage(chatId, `buy failed: ${data.message}`);
                                             } else {
                                                 console.log(
                                                     `The buy order request did not reach bittrex. Quantity: ${chunkQuantity}. Rate: ${buyRate}`
                                                 );
+                                                telegram.sendMessage(chatId, 'The buy order request did not reach bittrex')
                                             }
                                         });
+                                    /*
+                                     If the current block satisfies the quantity needed, a single limit buy
+                                     and limit sell request pair is sent and that's it
+                                    */
                                     } else if (isLastLoop) {
                                         console.log(`Last round, quant need to buy is ${quantToBuy}, quant have so far is ${quantHave}`);
 
@@ -110,7 +134,7 @@ bot.on('message', (msg) => {
                                             if (data.success) {
                                                 const buySuccessMsg = 'All of your buys went through!';
 
-                                                bot.sendMessage(chatId, buySuccessMsg);
+                                                telegram.sendMessage(chatId, buySuccessMsg);
                                                 console.log(buySuccessMsg);
 
                                                 bittrex.selllimit({
@@ -121,7 +145,7 @@ bot.on('message', (msg) => {
                                                     if (data.success) {
                                                         const finalSellSuccessMsg = `Your final sell of ${quantToBuy} ${market} went through at the rate of ${sellRate}`;
 
-                                                        bot.sendMessage(chatId, finalSellSuccessMsg);
+                                                        telegram.sendMessage(chatId, finalSellSuccessMsg);
                                                         console.log(finalSellSuccessMsg);
                                                     } else if (!data.success) {
                                                         console.log(`Something went wrong with the final sell: ${data.message}`);
@@ -134,7 +158,7 @@ bot.on('message', (msg) => {
                                                             if (data.success) {
                                                                 const finalSellSuccessMsg = `Your final sell of ${quantToBuy} ${market} went through at the rate of ${sellRate}`;
 
-                                                                bot.sendMessage(chatId, finalSellSuccessMsg);
+                                                                telegram.sendMessage(chatId, finalSellSuccessMsg);
                                                                 console.log(finalSellSuccessMsg);
                                                             } else if (!data.success) {
                                                                 console.log(`Something went wrong with the final sell: ${data.message}`);
@@ -146,7 +170,7 @@ bot.on('message', (msg) => {
                                                 });
                                             } else if (!data.success) {
                                                 console.log(`final buy failed: ${data.message}`);
-                                                bot.sendMessage(chatId, `final buy failed: ${data.message}`);
+                                                telegram.sendMessage(chatId, `final buy failed: ${data.message}`);
                                             } else {
                                                 console.log(
                                                     `The final buy order did not reach bittrex. Quantity: ${quantToBuy}, Rate: ${buyRate}`
@@ -156,33 +180,33 @@ bot.on('message', (msg) => {
                                     }
                                 }
                             }
+                            // initializes syncLoop function
                             syncLoop(data.result, quantHave);
                         }
                     );
+                // Handles the cases where the parameters are not met
                 } else if (data.result[0].Ask > (data.result[0].Low * lowCurrentPercent)
                     && data.result[0].High < (data.result[0].Low * highLowPercent)) {
                         const lowAskMsg = `The current ask price is ${data.result[0].Ask}, it is larger than the daily low times the percentage set: ${data.result[0].Low * lowCurrentPercent}, bot will not initiate`;
-                        bot.sendMessage(chatId, lowAskMsg);
-                        console.log(lowAskMsg);
+                        telegram.sendMessage(chatId, lowAskMsg);
                     } else if (data.result[0].Ask < (data.result[0].Low * lowCurrentPercent)
                     && data.result[0].High > (data.result[0].Low * highLowPercent)) {
                         const highLowMsg = `The daily high is ${data.result[0].High}, it is larger than the daily low times the percentage set: ${data.result[0].Low * highLowPercent}, bot will not initiate`;
-                        bot.sendMessage(chatId, highLowMsg);
-                        console.log(highLowMsg);
+                        telegram.sendMessage(chatId, highLowMsg);
                     } else {
                         const highLowLowAskMsg = 'Both daily high vs daily low and daily low vs current price tests were not passed, bot will not initiate';
-                        bot.sendMessage(chatId, highLowLowAskMsg);
-                        console.log(highLowLowAskMsg);
+                        telegram.sendMessage(chatId, highLowLowAskMsg);
                     }
-            } else {
+            // Handles the case where the market link entered is invalid
+                } else {
                 const marketErrMsg = 'Please enter a valid market link';
-                bot.sendMessage(chatId, marketErrMsg);
+                telegram.sendMessage(chatId, marketErrMsg);
             }
             });
+        // Handles the case where the user does not have enough fund to cover the buy
         } else {
             const insufficientFundMsg = `Your balance is ${data.result.Available}, it is less than the buy quantity you set up (${process.env.q}), bot will not initiate`;
-            bot.sendMessage(chatId, insufficientFundMsg);
-            console.log(insufficientFundMsg);
+            telegram.sendMessage(chatId, insufficientFundMsg);
         }
     });
 });
--- statics/telegramAPI_KEY.js
+++ statics/telegramAPI_KEY.js
@@ -3,6 +3,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 module.exports = new TelegramBot(
-    'YOUR_TELEGRAM_ACESS_TOKEN', 
+    'YOUR_TELEGRAM_ACCESS_TOKEN',
     {polling: true}
 );
